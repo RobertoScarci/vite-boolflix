692ae696497625f1def9c834365130ba328db57e
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-20 21:23:10 +0100

    individuazione delle formule per prendere il film una volta cercato nella barra con apposite lingua e recensioni

---
+++ my-vue-app/src/components/SingleFilm.vue
@@ -0,0 +1,47 @@
+<script>
+    import LangFlag from '../../node_modules/vue-lang-code-flags'
+
+    export default {
+    name: "SingleFilm",
+    methods: {
+        getStars(vote) {
+            let stars = ""
+            const starNumber = parseInt(vote / 2);
+            for(let i=0; i < starNumber; i++) {
+                stars += "★";
+            }
+            for(let i=0; i < 5 - starNumber; i++) {
+                stars += "☆";
+            }
+            return stars;
+
+        }
+    },
+    components: {
+        LangFlag
+    },
+    props: {
+        movie: {
+            type: Object,
+            required: true
+        }
+    }
+    }
+</script>
+
+<template>
+    <div class="card col-2 mb-3 me-3">
+        <img :src="(movie.poster_path != null) ? `http://image.tmdb.org/t/p/w342/${movie.poster_path}` : '/placeholder.jpg' " class="card-img-top mt-2" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ movie.title }}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Lingua: <lang-flag :iso="movie.original_language"/> </li>
+            <li class="list-group-item">Voto: {{ getStars(movie.vote_average) }} </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
