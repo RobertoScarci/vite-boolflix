aff8b12b8f680833851cf5c0d1e6e6997bdc956a
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-20 21:21:45 +0100

    implementazione della sezione Main

---
+++ my-vue-app/src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+    import ListFilm from './ListFilm.vue';
+
+    export default {
+    name: "AppMain",
+    components: {
+        ListFilm
+    }
+    }
+</script>
+
+<template>
+    <main>
+        <ListFilm/>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
