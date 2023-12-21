72409c8a4168e0b43e8740790625ed0169d6da40
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-21 12:36:27 +0100

    implementazione delle regole rigurardanti l'inserimento delle serie tv nella barra di ricerca

--- my-vue-app/src/components/AppHeader.vue
+++ my-vue-app/src/components/AppHeader.vue
@@ -20,29 +20,39 @@
                 }
             })
             .then( response => {
-                console.log(response);
                 this.store.movieList = response.data.results;
             })
-            .catch(function (error) {
-                console.log(error);
-            }); 
-        }
-    }
+        },
+        getTvSeries(query) {
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                api_key: '2902af90259528a807e51aa892d65a46',
+                query,
+                language: 'it-IT'
+                }
+            })
+            .then( response => {
+                this.store.tvList = response.data.results;
+            })
+        }}
     }
 </script>
 
 <template>
-    <nav class="navbar mb-4">
-        <div class="container-fluid bg-dark">
-            <a class="navbar-brand text-danger fw-bold fs-2">BoolFlix</a>
+    <nav class="navbar">
+        <div class="container-fluid ">
+            <a class="navbar-brand text-danger fw-bold fs-2 m-3"><img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt=""></a>
             <div class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="query" @keydown.enter="getFilm(query)">
-                <button class="btn btn-danger" @click="getFilm(query)">Search</button>
+                <input class="form-control me-3 pt-2" type="search" placeholder="Search" aria-label="Search" v-model="query" @keydown.enter="getFilm(query), getTvSeries(query)">
+                <button class="btn btn-danger" @click="getFilm(query), getTvSeries(query)">Search</button>
             </div>
         </div>
     </nav>
 </template>
 
 <style lang="scss" scoped>
+img{
+    width: 40%;
+}
 
 </style>
