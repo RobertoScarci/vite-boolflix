1820e114a8ddae8a49b1eabf11e7e80c11c461e9
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-22 10:50:41 +0100

    creazione e modifica dell'header con logo originale e sezione nav

--- my-vue-app/src/components/AppHeader.vue
+++ my-vue-app/src/components/AppHeader.vue
@@ -41,8 +41,17 @@
 <template>
     <nav class="navbar">
         <div class="container-fluid ">
-            <a class="navbar-brand text-danger fw-bold fs-2 m-3"><img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt=""></a>
-            <div class="d-flex" role="search">
+            
+            <div class="nav mb-3">
+                <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="" class="p-3">
+                <span class="mt-4 active">Home</span>
+                <span class="mt-4">Serie TV</span>
+                <span class="mt-4">Film</span>
+                <span class="mt-4">Originali</span>
+                <span class="mt-4">Aggiunti di recente</span>
+                <span class="mt-4">La mia lista</span>
+            </div>
+            <div class="d-flex mb-3" role="search">
                 <input class="form-control me-3 pt-2" type="search" placeholder="Search" aria-label="Search" v-model="query" @keydown.enter="getFilm(query), getTvSeries(query)">
                 <button class="btn btn-danger" @click="getFilm(query), getTvSeries(query)">Search</button>
             </div>
@@ -52,7 +61,21 @@
 
 <style lang="scss" scoped>
 img{
-    width: 40%;
+    width: 20%;
+    margin-right: 1rem;
+}
+
+div > span{
+    margin-right: 1.5rem;
+    color: #777777;
+}
+
+.active{
+    color: white;
+}
+
+span:hover{
+    color: white;
 }
 
 </style>
